7fb7d616cb2854062792cb95d3aa63922194e5eb
JhonDev365 <jhonarcila365@gmail.com>
2022-04-17 18:44:28 -0500

    Javascript code is added

---
+++ script.js
@@ -0,0 +1,56 @@
+'use strict'
+
+let secretNumber = Math.trunc(Math.random()*20)+1;
+let score = 20;
+let highscore = 0;
+
+
+
+document.querySelector('.check').addEventListener('click', function() {
+    const guess = Number(document.querySelector('.guess').value);
+    // When there is no input
+    if(!guess) {
+        document.querySelector('.message').textContent = 'No hay Número';
+    // When player wins    
+    }else if(guess === secretNumber) {
+        document.querySelector('.message').textContent = 'Número Correcto !';
+        document.querySelector('.number').textContent = secretNumber;
+        document.querySelector('body').style.backgroundColor = '#60b347';
+        document.querySelector('.number').style.width = '30rem';
+        if(score > highscore) {
+            highscore = score;
+            document.querySelector('.highscore').textContent = highscore;
+        }
+    // When guess is too high    
+    }else if(guess > secretNumber) {
+        if(score > 1) {
+            document.querySelector('.message').textContent = 'Demasiado Alto !';
+            score --;
+            document.querySelector('.score').textContent = score;
+        }else {
+            document.querySelector('.message').textContent = 'Game Over !';
+            document.querySelector('.score').textContent = 0;
+        }
+    // When guess is too low   
+    }else if(guess < secretNumber) {
+        if(score > 1) {
+            document.querySelector('.message').textContent = 'Demasiado Bajo !';
+            score --;
+            document.querySelector('.score').textContent = score;
+        }else {
+            document.querySelector('.message').textContent = 'Game Over !';
+            document.querySelector('.score').textContent = 0;
+        }
+    }
+})
+
+document.querySelector('.again').addEventListener('click', function() {
+    score = 20;
+    secretNumber = Math.trunc(Math.random()*20)+1;
+    document.querySelector('.message').textContent = 'Empieza Adivinar !';
+    document.querySelector('.score').textContent = score;
+    document.querySelector('.number').textContent = '?';
+    document.querySelector('.guess').value = '';
+    document.querySelector('body').style.backgroundColor = '#222';
+    document.querySelector('.number').style.width = '15rem';
+})
